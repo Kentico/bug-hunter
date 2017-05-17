bff36af61b95f186a3e7f542e2c516fb7edd6d74
Suzii <z.dankovcikova@gmail.com>
2017-05-17 19:33:30 +0200

    Add another command to SolutionStats.ps1

--- utils/SolutionStatistics/SolutionStats.ps1
+++ utils/SolutionStatistics/SolutionStats.ps1
@@ -1,2 +1,3 @@
 ﻿# Runs the console application SolutionStatistics over CMS solutoin (path hardcoded in app) and outputs the it to a file in current folder
 .\SolutionStatistics\bin\Release\SolutionStatistics.exe -OnlyProjectsWithInstalledAnalyzer > .\solutionStats-analyzerProjectsOnly.txt
+.\SolutionStatistics\bin\Release\SolutionStatistics.exe > .\solutionStats-allProjects.txt
